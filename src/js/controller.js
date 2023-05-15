58ab81ba31cc1ace4c91c25b03a23fe5c6baccc8
Everson Soares <eversonv4@gmail.com>
2023-05-15 11:27:40 -0300

    feat: project forkify, configured sass compiler, integrated APi endpoints to project, listing recipes, implemented MVC architecture

---
+++ src/js/controller.js
@@ -0,0 +1,40 @@
+import * as model from "./model";
+import recipeView from "./views/recipeView";
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+const recipeContainer = document.querySelector(".recipe");
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const recipeId = window.location.hash.slice(1);
+
+    if (!recipeId) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe
+    await model.loadRecipe(recipeId);
+
+    // Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+["hashchange", "load"].forEach((event) =>
+  window.addEventListener(event, controlRecipes)
+);
---
+++ src/js/model.js
@@ -0,0 +1,32 @@
+import { async } from "regenerator-runtime";
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (recipeId) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${recipeId}`
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    const { recipe } = data.data;
+
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_ime,
+      ingredients: recipe.ingredients,
+    };
+  } catch (error) {
+    alert(error);
+  }
+};
